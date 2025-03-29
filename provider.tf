6f35320beb1b8a49dd4960de7df247aa55e5445a
mastercam123 <k4mbingoce@yahoo.com>
2025-03-29 19:41:08 +0100

    Added secret for Atlantis.

--- provider.tf
+++ provider.tf
@@ -10,3 +10,10 @@ terraform {
     }
   }
 }
+
+provider "github" {
+  owner           = var.github_organization
+  id              = var.github_app_id
+  installation_id = var.github_app_installation_id
+  pem_file        = aws_secretmanager_secret_version.github_app_private_key.secret_string
+}
